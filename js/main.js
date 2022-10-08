cf355d9701e99ae688b1fb4ba82024001e8e7297
JasurKhayrullayev <jasurkhayrullayev2002@gmail.com>
2022-10-08 21:00:18 +0500

    yfghfh

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// string number bigInt boolean undefained null symbol object 
+
+// 1-masala
+
+// let arr = ["keep" , "remove", "keep", "remove"]; 
+// function remove(arr) {
+//     let newArr = [];
+//     for (let index = 0; index < arr.length; index++) {
+//         if ( index % 2 === 0) {
+//             newArr.push(arr[index]); 
+//         }
+//     }
+//     return newArr;
+// }
+// console.log(remove(arr));
+
+// 2-masala
+
